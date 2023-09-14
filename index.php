210b0b6768b4e0190a803accfe554dc820711e37
Beterraba <89883547+vittorrmedeiros@users.noreply.github.com>
2023-09-14 13:55:47 +0000

    edit

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Torre de Hanói</title>
 </head>
+
 <body>
 
     <form method="POST">
@@ -13,25 +15,28 @@
         <input type="submit" value="Enviar"> <br><br>
     </form>
 
-<?php
-    
-    function hanoi($n, $de, $para, $aux) {
-        if($n == 1) {
+    <?php
+
+    if ($_POST) {
+        $n = $_POST['n'];
+        $disco = $_POST['n'];
+        $equacao = (2 ** $disco) - 1;
+        echo "O mínimo de movimentos possíveis é: $equacao <br><br>";
+        hanoi($n, 'A', 'C', 'B');
+    }
+
+    function hanoi($n, $de, $para, $aux)
+    {
+        if ($n == 1) {
             echo "Mova o disco 1 de $de para $para <br>";
             return;
         }
-        hanoi($n-1, $de, $aux, $para);
+        hanoi($n - 1, $de, $aux, $para);
         echo "Mova o disco $n de $de para $para <br>";
-        hanoi($n-1, $aux, $para, $de);
+        hanoi($n - 1, $aux, $para, $de);
     }
-    
-    
-        if($_POST) {
-            $n = $_POST['n'];
-            hanoi($n, 'A', 'C', 'B');
-        }
 
-?>
+    ?>
+    
 </body>
 </html>
-
